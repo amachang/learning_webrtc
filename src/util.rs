b883d73f4fca792006091a3bab69c5e97d25096f
Hitoshi Amano <seijro@gmail.com>
2024-02-28 16:23:11 +0900

    revert

--- src/util.rs
+++ src/util.rs
@@ -37,7 +37,7 @@ use webrtc::mux::{self, Mux, mux_func};
 use rtcp::payload_feedbacks::picture_loss_indication::PictureLossIndication;
 use sha2::{Sha256, Digest};
 
-const RECEIVE_MTU: usize = 1220;
+const RECEIVE_MTU: usize = 1460;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum DtlsRole {
